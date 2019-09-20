e185a9b1d75a9ae20bb7a4a847001622134bd6a1
dezert99 <bubbamart99@gmail.com>
2019-09-20 16:58:07 -0400

    Undid babel upgrade

--- demo.config.js
+++ demo.config.js
@@ -35,7 +35,7 @@ module.exports = {
             exclude: /(node_modules|bower_components|build)/,
             loader: 'babel-loader',
             options:{
-                presets: ['@babel/env']
+                presets: ['env']
             }
           },
           {
